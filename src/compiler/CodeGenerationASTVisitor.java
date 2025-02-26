aae39b531ee237636145b0899b83ad74dff6f894
Alessandro Stefani <alessandro.stefani10@studio.unibo.it>
2025-02-26 11:35:55 +0100

    fix graterEqual

--- src/compiler/CodeGenerationASTVisitor.java
+++ src/compiler/CodeGenerationASTVisitor.java
@@ -146,10 +146,14 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
         if (print) printNode(n);
         String l1 = freshLabel();
         String l2 = freshLabel();
+        String l3 = freshLabel();
+
         return nlJoin(
                 visit(n.left),
                 visit(n.right),
+                "beq " + l3,
                 "bleq " + l1, //?
+                l3 + ":",
                 "push 1",
                 "b " + l2,
                 l1 + ":",
